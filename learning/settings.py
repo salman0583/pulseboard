62645553d6d5bb06030fae1ee91bee052ff5dcc8
Your Name <salman0583>
2026-03-04 12:33:28 +0530

    small change

--- learning/settings.py
+++ learning/settings.py
@@ -23,8 +23,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1").split(",")
-
+ALLOWED_HOSTS = os.environ.get( "ALLOWED_HOSTS","127.0.0.1,localhost,pulseboard-uik0.onrender.com").split(",")
 
 # ======================================================
 # OTP CONFIG
